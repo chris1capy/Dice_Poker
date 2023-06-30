1ef337ca9be7c30545f93885657cf2b656b09c83
chris1capy <krzysztof.ost@outlook.com>
2023-06-30 10:21:33 +0200

    Added name choice and output clear

--- Dice_Poker.py
+++ Dice_Poker.py
@@ -1,42 +1,6 @@
 import random
+from os import system, name
 
-'''
-def print_dice(dice_list):
-
-    # Prints dices one by one, refering to their coresponding numbers from the list used as an argument
-    
-    patterns = {
-                'top/bot':' -------',
-                'none':'|       |',
-                "one":'|   *   |',
-                'two':'| *   * |',
-                'one_right':'|     * |',
-                'one_left':'| *     |'
-                }
-    
-    dices = {
-             1:['top/bot','none','one','none','top/bot'],
-             2:['top/bot','none','two','none','top/bot'],
-             3:['top/bot','one_left','one','one_right','top/bot'],
-             4:['top/bot','two','none','two','top/bot'],
-             5:['top/bot','two','one','two','top/bot'],
-             6:['top/bot','two','two','two','top/bot']
-             }
-    
-    for dice in dices[dice_list]:
-        print(patterns[dice])
-
-'''
-
-'''
-def print_dice_rows(dice_list):
-
-    # Prints all the dices from privided list, using print_dice() function
-
-    for dice in dice_list:
-        print_dice(dice)
-
-'''
 
 def return_dice_rows(dice_num):
 
@@ -96,6 +60,8 @@ def print_dice_rows(dice_list):
 
     for num in range(0,5):
         print(dice_dict[num])
+
+    print()
 
 def pick_starting_player():
 
@@ -208,8 +174,46 @@ def roll_combination_check(given_list):
     print(roll_value_list)
     return [combination,combined_value]
 
+def clear():
+    
+    #https://www.geeksforgeeks.org/clear-screen-python/
 
+    # Windows
+    if name == 'nt':
+        _ = system('cls')
+ 
+    # MacOS or Linux
+    else:
+        _ = system('clear')
 
+def change_name(players_name_dict, player_name_code):
+
+    print(player_name_code + " you can choose your nickname that will be displayed during the game.")
+
+    nickname = input("Your nickname (you can leave it empty if you just want to be called '{}'): ".format(player_name_code))
+
+    players_name_dict[player_name_code] = nickname
+
+    clear()
+    print(player_name_code + " now will be called " + players_name_dict[player_name_code] + ".\n")
+
+    if player_name_code == "Player 1":
+        player_name_code = "Player 2"
+    else:
+        player_name_code = "Player 1"
+
+    print("Now, " + player_name_code + " you can choose your nickname too.")
+
+    nickname = input("Your nickname (you can leave it empty if you just want to be called '{}'): ".format(player_name_code))
+
+    players_name_dict[player_name_code] = nickname
+
+    clear()
+    print(player_name_code + " now will be called " + players_name_dict[player_name_code] + ".")
+    
+
+# A string to be placed on top of the dices, to help players choose dices to roll
+dice_number_help = "   [1]        [2]        [3]        [4]        [5]   "
 
 
 
@@ -218,36 +222,58 @@ def roll_combination_check(given_list):
 
 while True:
 
-    # A string to be placed on top of the dices, to help players choose dices to roll
-    dice_number_help = "   [1]        [2]        [3]        [4]        [5]   "
+    clear()
     
-    print("Welcome to The Dice Poker game! The rules originate from video game The Witcher 2: Assassins of Kings.")
+    print("Welcome to The Dice Poker game! The rules originate from video game The Witcher 2: Assassins of Kings.\n\n\n")
 
-    players_set = {
+    players_dice_dict = {
                 "Player 1" : [0,0,0,0,0],
                 "Player 2" : [0,0,0,0,0]
+               }
+    
+    players_name_dict = {
+                "Player 1" : "",
+                "Player 2" : ""
+               }
+    
+    players_score_dict = {
+                "Player 1" : {},
+                "Player 2" : {}
                }
 
     print("Person pressing the Enter key will be the Player 1. The game decide which person will go first.")
     input("Press Enter when you ready!")
 
+    clear()
+
     current_player = pick_starting_player()
-    print(current_player + " will be the first to roll!")
-    input("Press Enter when you're ready to roll!")
+    print(current_player + " will be the first to roll!\n")
+
+    change_name(players_name_dict, current_player)
+
+    input("{} press Enter when you're ready to roll!".format(players_name_dict[current_player]))
 
     turn = 1
 
     while turn == 1:
 
-        roll_all_dices(players_set[current_player])
+        roll_all_dices(players_dice_dict[current_player])
 
+        clear()
+
+        print(players_name_dict[current_player] + " Dices.")
+        print()
         print(dice_number_help)
         print()
+        print_dice_rows(players_dice_dict[current_player])
 
-        print_dice_rows(players_set[current_player])
+        roll_choosen_dices(reroll_choice(), players_dice_dict[current_player])
 
-        roll_choosen_dices(reroll_choice(), players_set[current_player])
-        print_dice_rows(players_set[current_player])
+        clear()
+
+        print(players_name_dict[current_player] + " Dices.")
+        print()
+        print_dice_rows(players_dice_dict[current_player])
 
         if current_player == "Player 1":
             current_player = "Player 2"
@@ -258,35 +284,88 @@ while True:
 
     while turn == 2:
 
-        print("Now is " + current_player + " Turn!")
+        print("Now is " + players_name_dict[current_player] + " Turn!")
 
-        input("Press Enter when you're ready to roll!")
+        input("{} press Enter when you're ready to roll!".format(players_name_dict[current_player]))
+        clear()
 
-        roll_all_dices(players_set[current_player])
+        roll_all_dices(players_dice_dict[current_player])
 
+        print(players_name_dict[current_player] + " Dices.")
+        print()
+        
         print(dice_number_help)
         print()
 
-        print_dice_rows(players_set[current_player])
+        print_dice_rows(players_dice_dict[current_player])
 
-        roll_choosen_dices(reroll_choice(), players_set[current_player])
-        print_dice_rows(players_set[current_player])
+        roll_choosen_dices(reroll_choice(), players_dice_dict[current_player])
+
+        clear()
+
+
+        print(players_name_dict[current_player] + " Dices.")
+        print()
+        print_dice_rows(players_dice_dict[current_player])
 
 
         turn += 1
 
     print("That concludes the game! Compare your configurations to see which player won!")
 
-    input("Press Enter to go to the end screen")
+    input("Press Enter to go to the end screen...")
+
+    clear()
 
     print("For a reminder, there are your dices configurations side by side.")
 
-    print("Player 1 dices.")
-    print_dice_rows(players_set["Player 1"])
+    print(players_name_dict["Player 1"] + " Dices.")
+    print_dice_rows(players_dice_dict["Player 1"])
 
-    print("Player 2 dices.")
-    print_dice_rows(players_set["Player 2"])
+    print(players_name_dict["Player 2"] + " Dices.")
+    print_dice_rows(players_dice_dict["Player 2"])
 
     break
 
 input('Press Enter to exit...')
+
+
+
+
+'''
+def print_dice(dice_list):
+
+    # Prints dices one by one, refering to their coresponding numbers from the list used as an argument
+    
+    patterns = {
+                'top/bot':' -------',
+                'none':'|       |',
+                "one":'|   *   |',
+                'two':'| *   * |',
+                'one_right':'|     * |',
+                'one_left':'| *     |'
+                }
+    
+    dices = {
+             1:['top/bot','none','one','none','top/bot'],
+             2:['top/bot','none','two','none','top/bot'],
+             3:['top/bot','one_left','one','one_right','top/bot'],
+             4:['top/bot','two','none','two','top/bot'],
+             5:['top/bot','two','one','two','top/bot'],
+             6:['top/bot','two','two','two','top/bot']
+             }
+    
+    for dice in dices[dice_list]:
+        print(patterns[dice])
+
+'''
+
+'''
+def print_dice_rows(dice_list):
+
+    # Prints all the dices from privided list, using print_dice() function
+
+    for dice in dice_list:
+        print_dice(dice)
+
+'''
